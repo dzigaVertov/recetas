bf3127f6cea632398efdd78e6790c39b64bc4b75
Marcelo Gómez <marcelo@pop-os.localdomain>
2023-01-04 23:50:35 -0300

    Updates

---
+++ precache-manifest.24bcca7d4bfa8cb9cbb65b66fd15ce8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd95296d4c6043357847",
+    "url": "/recetas/static/css/main.02241663.chunk.css"
+  },
+  {
+    "revision": "fd95296d4c6043357847",
+    "url": "/recetas/static/js/main.fd95296d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/recetas/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/recetas/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "02980590352a4d2ccc40",
+    "url": "/recetas/static/js/runtime~main.02980590.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/recetas/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "42b6da733ee008b31301669d1d7546e6",
+    "url": "/recetas/index.html"
+  }
+];
