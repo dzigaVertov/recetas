8fce08dd9a33a54cd14dc66879dda111d4e3e21d
Marcelo Gómez <marcelo@pop-os.localdomain>
2023-01-27 17:02:26 -0300

    cambio pc

--- src/Receta.js
+++ src/Receta.js
@@ -2,18 +2,20 @@ import React from 'react';
 import Ingredientes from './Ingredientes.js';
 import recetas_db from './Receta_db.js';
 import {useParams} from "react-router-dom";
-import {Card, Button, Container} from 'react-bootstrap';
-
+import {Card, Button, Container, Image} from 'react-bootstrap';
+import gato from './assets/cute-cat-photos-1593441022.jpg';
 
 export default function Receta(props) {
   const {id} = useParams();
-  const receta = recetas_db[id];
+    const receta = recetas_db[id];
+
+
   return (
       <Container className='d-grid gap-3'>
+        <Image src={gato}/>
       <Card >
-
         <Card.Body>
-          <Card.Img variant='right' src='receta.'/>
+          <Card.Img variant='top' src={require('./assets/cute-cat-photos-1593441022.jpg')}/>
             <Card.Title>{receta.nombre}</Card.Title>
             <Card.Subtitle>Ingredientes</Card.Subtitle>
             <Card.Text>
