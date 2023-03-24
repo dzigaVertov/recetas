ac1131cd61d3b5f73d71248e74edf64bc53d2954
Marcelo Demian Gómez <marcelo@pop-os.localdomain>
2023-03-24 18:53:42 -0300

    completadas tarjetas lista home

--- src/Catbar.js
+++ src/Catbar.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Category from './Category.js';
 
 
 const Catbar = ({ filtrar }) => {
+
+    const [scrollTop, setScrollTop] = useState(0);
+
+    const scrolledCatBar = {
+        marginTop: '4rem',
+        transition: 'margin-top 1s',
+    };
+
+    const scrolledCategories = {
+        width: '2rem',
+        transition: 'width 1s',
+    };
+
+    useEffect(()=> {
+        const onscroll = e => {
+            setScrollTop(e.target.scrollTop);
+        };
+        const sc = document.querySelector('#root');
+        sc.addEventListener('scroll', onscroll);
+
+        return () => {
+            sc.removeEventListener('scroll', onscroll);
+        };
+    }, [scrollTop]);
+    
     return (
-        <div className='catbar'>
-            <Category />
+        <div className='catbar' style={scrollTop > 0 ? scrolledCatBar : {}} >
+            <Category style={scrollTop > 0 ? scrolledCategories : {}} />
             <Category />
             <Category />
             <Category />
