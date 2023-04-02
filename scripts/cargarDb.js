ea4f1aa343ee1a0c02ac2de12ce0517accd99229
Marcelo Demian Gómez <marcelo@pop-os.localdomain>
2023-04-01 21:59:53 -0300

    agregada extracción de datos de receta de json para base de datos

--- scripts/cargarDb.js
+++ scripts/cargarDb.js
@@ -1,70 +1,67 @@
 import fs from 'fs/promises';
 
-
-
 const jsonDb = await fs.readFile('../db.json', { encoding: 'utf8' });
 const archivoJson = await JSON.parse(jsonDb);
 
-let primerNivel = archivoJson.contents[1];
-
-// // Ingredientes table
-// console.log(primerNivel.contents[3].properties.title[0]);
-// // 100gs
-// console.log(primerNivel.contents[3].contents[0].contents[0].contents[4].contents[1].contents[0]);
-// // 135gs
-
-// console.log(primerNivel.contents[3].contents[0].contents[0].contents[3].contents[1].contents[0]);
-// // 500gs
-// console.log(primerNivel.contents[3].contents[0].contents[0].contents[2].contents[1].contents[0]);
-
-// // 100gs
-// console.log(primerNivel.contents[3].contents[0].contents[0].contents[4].contents[0].contents[0]);
-// // 135gs
-// console.log(primerNivel.contents[3].contents[0].contents[0].contents[3].contents[0].contents[0]);
-// // 500gs
-// console.log(primerNivel.contents[3].contents[0].contents[0].contents[2].contents[0].contents[0]);
-
-// let encabezados = [];
-// primerNivel.contents.forEach(elem=>(encabezados.push(elem.properties['raw-value'])));
-// encabezados.forEach(elem => {if(elem) console.log(elem);});
-
-// let textos = [];
-// primerNivel.contents.forEach(elem=> {
-//     if ( elem.contents[0].contents[0]){
-//         textos.push(elem.contents[0].contents[0]);
-//     } else {
-//         elem.contents.forEach(elem => console.log(elem.properties['raw-value']));
-//     }
+// console.log(getCategoria(archivoJson, 'Nombre'));
+// console.log(getCategoria(archivoJson, 'Instrucciones'));
+// console.log(getCategoria(archivoJson, 'Observaciones'));
+// console.log(getCategoria(archivoJson, 'Ingredientes'));
+let obmousse = getReceta(archivoJson, 'Mousse de chocolate');
+console.log(getCategoria(obmousse, 'Ingredientes'));
 
 
-// });
-
-
-// textos.forEach(elem => {
-//     if(elem) {
-//         console.log(elem.contents[0]);
-//         if (elem.properties.contents){
-//             console.log(elem.properties.contents);
-
-//         } else {
-//             console.log('no hay');
-//         }
-//     }
-// });
-
-console.log(getCategoria(archivoJson, 'Nombre'));
-console.log(getCategoria(archivoJson, 'Instrucciones'));
-console.log(getCategoria(archivoJson, 'Observaciones'));
+// Equivalente al zip de python (no chequea cuál de los arrays es el más largo)
+function zip(arrays) {
+    return arrays[0].map((_, idx) => {
+        return arrays.map(array => array[idx]);
+    });
+}
 
 function getCategoria(ob, encabezado) {
     if (!encabezado) return ob;
+
     if (ob.contents) {
         for (const nd of ob.contents) {
             if (nd.properties && nd.properties.title && (nd.properties.title[0] === encabezado)) {
-                return nd.contents[0].contents[0].contents[0];
+                const raiz = nd.contents[0].contents[0];
+
+                if (encabezado === 'Ingredientes') return getTablaIngredientes(raiz);
+                return raiz.contents[0];
             }
+
             let rec = getCategoria(nd, encabezado);
-            if(rec) return rec;
+            if (rec) return rec;
+        }
+    }
+}
+
+function getTablaIngredientes(raiz) {
+
+    let ingredientes = raiz.contents
+        .filter(elem => (elem.contents.length > 0))
+        .map(elem => elem.contents
+            .map(elem2 => elem2.contents[0])[0]);
+
+    let cantidades = raiz.contents
+        .filter(elem => (elem.contents.length > 0))
+        .map(elem => elem.contents
+            .map(elem2 => elem2.contents[0])[1]);
+
+    let pares = zip([ingredientes, cantidades]);
+    let ob = {};
+    pares.forEach(elem => ob[String(elem[0])] = elem[1]);
+    return ob;
+}
+
+function getReceta(ob, nombre) {
+    for (let nd of ob.contents) {
+        if (nd.properties && nd.properties.title && (nd.properties.title[0] === nombre)) {
+            return nd;
+        }
+        if (nd.contents) {
+            let rec = getReceta(nd, nombre);
+            if (rec) return rec;
         }
     }
 }
